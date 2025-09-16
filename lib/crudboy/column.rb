7e14915ba98d88511fad8e6b83656f37e3ac9b46
Liu Xiang <liuxiang921@gmail.com>
2025-09-16 16:04:08 +0800

    Add Python type support in Crudboy column handling

--- lib/crudboy/column.rb
+++ lib/crudboy/column.rb
@@ -37,6 +37,24 @@ module Crudboy
       "decimal" => 'DECIMAL'
     }
 
+    PYTHON_TYPES = {
+      "varchar" => 'str',
+      "char" => 'str',
+      "text" => 'str',
+      "longtext" => 'str',
+      "int" => 'int',
+      "smallint" => 'int',
+      "bigint" => 'int',
+      "tinyint" => 'int',
+      "double" => 'float',
+      "date" => 'datetime',
+      "datetime" => 'datetime',
+      "timestamp" => 'datetime',
+      "time" => 'datetime',
+      "blob" => 'bytes',
+      "decimal" => 'decimal'
+    }
+
     attr_accessor :active_record_column, :primary
 
     def initialize(column, primary)
@@ -85,6 +103,67 @@ module Crudboy
     def jdbc_type
       raw_type = sql_type.scan(/^\w+/).first
       JDBC_TYPES[raw_type]
+    end
+
+    def python_type
+      raw_type = sql_type.scan(/^\w+/).first
+      PYTHON_TYPES[raw_type]
+    end
+
+    def python_type_with_optional
+      raw_python_type = python_type
+      null ? "Optional[#{raw_python_type}]" : raw_python_type
+    end
+
+    def py_sqlmodel_primary_column_declaration
+      format('%s: %s = Field(default_factory=gen_id, primary_key=True, max_length=%s, description="%s")', name, python_type, limit, comment)
+    end
+
+    def py_dto_column_declaration(optional = false)
+      # for example:     id: str = Field(description="ID")
+      if optional
+        format('%s: %s | None = Field(description="%s", default=None)', name, python_type, comment)
+      else
+        format('%s: %s = Field(description="%s")', name, python_type, comment)
+      end
+    end
+
+    def py_sqlmodel_regular_column_declaration
+      if created_at_column?
+        return py_sqlmodel_created_at_column_declaration
+      end
+
+      if updated_at_column?
+        return py_sqlmodel_updated_at_column_declaration
+      end
+
+      format('%s: %s = Field(default=None, max_length=%s, description="%s")', name, python_type_with_optional, limit || 'None', comment || '')
+    end
+
+    def py_sqlmodel_created_at_column_declaration
+      #  create_time: Optional[datetime] = Field(
+      #     default_factory=datetime.now,
+      #     description="Create Time",
+      #     sa_column_kwargs={"server_default": sa.func.now()},
+      # )
+      format('%s: %s = Field(default_factory=datetime.now, description="%s", sa_column_kwargs={"server_default": sa.func.now()})', name, python_type_with_optional, comment || '')
+    end
+
+    def py_sqlmodel_updated_at_column_declaration
+      #  update_time: Optional[datetime] = Field(
+      #     default_factory=datetime.now,
+      #     description="Update Time",
+      #     sa_column_kwargs={"server_default": sa.func.now()},
+      # )
+      format('%s: %s = Field(default_factory=datetime.now, description="%s", sa_column_kwargs={"server_default": sa.func.now()})', name, python_type_with_optional, comment || '')
+    end
+
+    def created_at_column?
+      sql_type =~ /datetime|time|date|timestamp/ && name =~ /gmt_created|created_at|create_time|create_date/
+    end
+
+    def updated_at_column?
+      sql_type =~ /datetime|time|date|timestamp/ && name =~ /gmt_modified|updated_at|update_time|update_date/
     end
 
     def method_missing(method, *args, **options, &block)
